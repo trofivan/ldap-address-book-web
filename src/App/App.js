98c46521d9686d728faab2daa22c483d8e0a57b7
Ivan Trofimov <ivan@trofimov.link>
2018-10-05 20:27:23 +0500

    feat: added blueprint, removed old code

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Spinner, Intent } from '@blueprintjs/core';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Spinner intent={Intent.SUCCESS}/>
+  </div>
+);
+
+export default App;
