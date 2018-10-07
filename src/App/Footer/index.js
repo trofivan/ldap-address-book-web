fc13f6a644d8023f49aeb2580f62a2f3c082fef8
Ivan Trofimov <ivan@trofimov.link>
2018-10-08 00:01:19 +0500

    feat(Content): delivered into separate component

--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import styles from './Footer.module.scss';
 
-export default () => <footer className={styles.footer}> Footer</footer>
+export default () => <footer className={styles.footer}> Footer</footer>;
