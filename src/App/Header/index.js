1908a5c4096799aa1f86c0254e5ff15152192339
Ivan Trofimov <ivan@trofimov.link>
2018-10-08 11:18:20 +0500

    feat(Header): added shadow

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import { Button, Navbar, Classes } from '@blueprintjs/core';
+import { Alignment } from '@blueprintjs/core/lib/cjs/common/alignment';
 
-export default () => <header className={styles.header}>App/Header/index.js</header>;
+export default () => (
+  <header className={styles.header}>
+    <Navbar>
+      <Navbar.Group align={Alignment.LEFT}>
+        <Navbar.Heading>This is Heading</Navbar.Heading>
+      </Navbar.Group>
+      <Navbar.Group align={Alignment.RIGHT}>
+        <Button className={Classes.MINIMAL} icon="help"/>
+      </Navbar.Group>
+    </Navbar>
+  </header>
+);
