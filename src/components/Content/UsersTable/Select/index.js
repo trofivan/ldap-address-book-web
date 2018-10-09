624e1359e538ef1ee5de9da8bbc3c80d578bb773
Ivan Trofimov <ivan@trofimov.link>
2018-10-09 22:08:49 +0500

    sync

--- src/components/Content/UsersTable/Select/index.js
+++ src/components/Content/UsersTable/Select/index.js
@@ -3,44 +3,56 @@ import { Button, Menu, MenuItem } from '@blueprintjs/core';
 import { Select } from '@blueprintjs/select';
 import styles from './Select.module.scss';
 
-const renderItem = (itemsTextAttr) => (item, { handleClick, modifiers, query }) => (
-  <MenuItem
-    key={item.id}
-    onClick={handleClick}
-    text={item[itemsTextAttr]}
-  />
-);
+export default ({ items = [], current = null, icon, name = '', itemsTextAttr = 'name' }) => {
+  const renderItem = (item, { handleClick, modifiers, query }) => (
+    <MenuItem
+      key={item.id}
+      onClick={handleClick}
+      text={item[itemsTextAttr]}
+    />
+  );
 
-const renderList = ({ items, query, renderItem }) => {
-  const renderedItems = items
-    .filter((el, indx) => indx < 10)
-    .map(renderItem);
+  const renderList = ({ items, query, renderItem }) => {
+    const renderedItems = items
+      .filter((el, indx) => indx < 10)
+      .map(renderItem);
+
+    return (
+      <Menu>
+        <MenuItem
+          text={renderedItems.length === items.length ? renderItem.length : `${renderedItems.length}/${items.length}`}
+          disabled={true}/>
+        {renderedItems}
+      </Menu>
+    );
+  };
+
+  const handleItemSelect = (item) => {
+    // console.log(item);
+  };
+
+  const filterItems = (query, item) => {
+    return false;
+    // console.log(query, item)
+  };
 
   return (
-    <Menu>
-      <MenuItem
-        text={renderedItems.length === items.length ? renderItem.length : `${renderedItems.length}/${items.length}`}
-        disabled={true}/>
-      {renderedItems}
-    </Menu>
+    <Select
+      items={items}
+      itemPredicate={filterItems}
+      itemRenderer={renderItem}
+      itemListRenderer={renderList}
+      onItemSelect={handleItemSelect}
+      popoverProps={{
+        // className: 'myClassName',
+
+        popoverClassName: styles.popover,
+        portalClassName: 'myPortalClassName'
+        // targetClassName: styles['popover-target']
+      }}
+    >
+      <Button text={name} icon={icon} fill={true} rightIcon="caret-down"/>
+    </Select>
   );
-};
 
-const handleItemSelect = (item) => {
-  console.log(item);
-};
-
-export default ({ items = [], current = null, icon, name = '', itemsTextAttr = 'name' }) => (
-  <Select
-    items={items}
-    itemRenderer={renderItem(itemsTextAttr)}
-    itemListRenderer={renderList}
-    onItemSelect={handleItemSelect}
-    popoverProps={{
-      popoverClassName: styles.popover,
-      targetClassName: styles['popover-target']
-    }}
-  >
-    <Button text={name} icon={icon} fill={true} rightIcon="caret-down"/>
-  </Select>
-);
+}
