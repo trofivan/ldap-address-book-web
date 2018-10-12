4ea5429fe9cbe249ff977d43a94e8be98bd15fdd
Ivan Trofimov <ivan@trofimov.link>
2018-10-12 15:47:46 +0500

    feat(App.js): removed test code

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,8 @@ import Header from './Header/index';
 import Content from './Content/index';
 import Footer from './Footer/index';
 
-/**
- * @TODO: temp
- */
-import TestBtn from '../containers/Test';
-
 const App = () => (
   <section className={styles.app}>
-    <TestBtn />
     <Header/>
     <Content/>
     <Footer/>
