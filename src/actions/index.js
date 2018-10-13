062cc3096262218ee61be41d7e2c9cea8213313e
Ivan Trofimov <ivan@trofimov.link>
2018-10-13 18:23:03 +0500

    feat(actions/index.js): added constants for fetching users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
+/**
+ * Filter
+ */
 export const FILTER_SET = 'FILTER_SET';
 
 export const actionSetFilter = (filter) => ({
   type: FILTER_SET,
   filter
 });
+
+/**
+ *  Users fetching
+ */
+export const USERS_FETCH_REQUEST = 'USERS_FETCH_REQUEST';
+export const USERS_FETCH_SUCCESS = 'USERS_FETCH_SUCCESS';
+export const USERS_FETCH_ERROR = 'USERS_FETCH_ERROR';
