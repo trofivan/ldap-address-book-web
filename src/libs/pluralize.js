95e6fd10076c6f61c473c87295b9c176633be9c1
Ivan Trofimov <ivan@trofimov.link>
2018-10-13 17:53:38 +0500

    feat(libs/plaralize.js): added pliralize lib

---
+++ src/libs/pluralize.js
@@ -0,0 +1,6 @@
+const pluralize = (number, words) => {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return `${number} ${words[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[Math.min(number % 10, 5)]]}`;
+};
+
+export default pluralize;
