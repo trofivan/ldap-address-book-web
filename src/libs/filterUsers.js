93f619106f3dc2a8fa2f7b365349a839729f819e
Ivan Trofimov <ivan@trofimov.link>
2018-10-16 09:01:34 +0500

    refactor(filterUsers.js)

--- src/libs/filterUsers.js
+++ src/libs/filterUsers.js
@@ -1,7 +1,6 @@
 export default (users = [], filter = {}) =>
   users
     .filter(user => {
-
       for (const key in filter) {
         if (filter[key] !== '' && filter[key] !== user[key]) {
           return false;
